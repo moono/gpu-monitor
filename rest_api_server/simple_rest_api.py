073d77a187a934f5ab1e43f5fa7de0a82ecb9feb
moono.song <moono.song@ncsoft.com>
2019-07-18 11:05:59 +0900

    try to solve logging problem

--- rest_api_server/simple_rest_api.py
+++ rest_api_server/simple_rest_api.py
@@ -6,8 +6,12 @@ from flask_restful import Resource, Api
 from nvidia_smi import run_nvidia_smi
 from information_organizer import reorganize_and_merge_info_chartjs
 
-
+# logging settings
 logging.basicConfig()
+logging.getLogger('apscheduler').setLevel(logging.ERROR)
+logging.getLogger('werkzeug').setLevel(logging.ERROR)
+
+# global variable
 parsed_gpu_info = None
 
 
@@ -21,13 +25,12 @@ def sensor():
     return
 
 
-logging.getLogger('apscheduler').setLevel(logging.ERROR)
+# background cron job
 sched_background = BackgroundScheduler(daemon=True)
 sched_background.add_job(sensor, 'interval', seconds=1)
 sched_background.start()
 
-
-logging.getLogger('werkzeug').setLevel(logging.ERROR)
+# flask app
 app = Flask(__name__)
 api = Api(app)
 
